ebd73cdfc98c19d0344270376fc418b9618b90d2
Tais Brogaard Pøhl <tais0109@student.eadania.dk>
2018-03-26 13:38:43 +0200

    more or less done

--- TestProject/Source/TestProject/MyStarSave.cpp
+++ TestProject/Source/TestProject/MyStarSave.cpp
@@ -1,8 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyStarSave.h"
+#include <direct.h>
 
 using namespace std;
+
+int stars[];
 
 MyStarSave::MyStarSave()
 {
@@ -10,4 +13,15 @@ MyStarSave::MyStarSave()
 
 MyStarSave::~MyStarSave()
 {
+}
+
+
+void OnCollide()
+{
+	
+
+	mkdir("c:/myfolder");
+
+
+	
 }

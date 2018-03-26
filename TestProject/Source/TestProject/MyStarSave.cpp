30da5e176de4c0465850aa070e09821ced37c9be
Tais Brogaard Pøhl <tais0109@student.eadania.dk>
2018-03-26 12:26:39 +0200

    star save c++

---
+++ TestProject/Source/TestProject/MyStarSave.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyStarSave.h"
+
+using namespace std;
+
+MyStarSave::MyStarSave()
+{
+}
+
+MyStarSave::~MyStarSave()
+{
+}

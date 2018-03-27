afb38fef2bfaf37a553fd6267eaa82477845e135
Zackian <steffentidus@hotmail.com>
2018-03-27 13:29:33 +0200

    something

---
+++ TestProject/Source/TestProject/SaveStarsThing.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SaveStarsThing.h"
+
+
+USaveStarsThing::USaveStarsThing()
+{
+	SaveSlotName = TEXT("TestSaveSlot");
+	UserIndex = 0;
+}
+

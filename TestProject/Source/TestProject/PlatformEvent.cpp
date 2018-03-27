8d120210d8ccfd36d9cffbf6f686083307e626b6
Patrick Nielsen <archi1990@hotmail.com>
2018-03-27 20:17:25 +0200

    level

--- TestProject/Source/TestProject/PlatformEvent.cpp
+++ TestProject/Source/TestProject/PlatformEvent.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+#include "PlatformEvent.h"
+
 #include "Gameframework/Actor.h"
 
-#include "PlatformEvent.h"
 
 
 // Sets default values for this component's properties
